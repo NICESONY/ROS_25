35349a38bc11c2c8ca18ca63442efcf93cef4d84
NICESONY <songunhee5426@gmail.com>
2025-06-30 16:01:27 +0900

    update

---
+++ colcon_ws/build/turtlebot3_simulations/colcon_command_prefix_build.sh
@@ -0,0 +1,5 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ros25/ROS/ROS_25/colcon_ws/install/turtlebot3_gazebo/share/turtlebot3_gazebo/package.sh"
+. "/home/ros25/ROS/ROS_25/colcon_ws/install/turtlebot3_manipulation_gazebo/share/turtlebot3_manipulation_gazebo/package.sh"
+. "/home/ros25/ROS/ROS_25/colcon_ws/install/turtlebot3_msgs/share/turtlebot3_msgs/package.sh"
+. "/home/ros25/ROS/ROS_25/colcon_ws/install/turtlebot3_fake_node/share/turtlebot3_fake_node/package.sh"
