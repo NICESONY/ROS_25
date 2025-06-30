ef2f1148803032a68f53d509b8eba39c5084588f
NICESONY <songunhee5426@gmail.com>
2025-06-30 17:11:34 +0900

    slam map save

--- colcon_ws/src/turtlebot3_controller/setup.py
+++ colcon_ws/src/turtlebot3_controller/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'sensor_listener = turtlebot3_controller.sensor_listener:main',
         ],
     },
 )
